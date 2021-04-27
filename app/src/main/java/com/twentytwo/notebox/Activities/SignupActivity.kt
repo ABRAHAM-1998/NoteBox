abd1e3989f17cb23959635eeaddc885bfbc85e2c
AFTERDEATH-1998 <abrahamjolly3377@gmail.com>
2021-04-27 22:00:40 +0530

    something newe expessila

--- app/src/main/java/com/twentytwo/notebox/Activities/SignupActivity.kt
+++ app/src/main/java/com/twentytwo/notebox/Activities/SignupActivity.kt
@@ -3,14 +3,12 @@ package com.twentytwo.notebox.Activities
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.twentytwo.notebox.R
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
@@ -19,12 +17,14 @@ import kotlinx.android.synthetic.main.activity_signup.*
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private val bugdf = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         auth = FirebaseAuth.getInstance()
+
 
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
 
@@ -49,7 +49,7 @@ class SignupActivity : AppCompatActivity() {
                 sgn_name.error = "please enter name"
                 sgn_name.requestFocus()
                 return@setOnClickListener
-            } else if(sgn_name.text.toString().length <=3) {
+            } else if (sgn_name.text.toString().length <= 3) {
                 sgn_name.error = "please enter valid name"
                 sgn_name.requestFocus()
                 return@setOnClickListener
@@ -88,26 +88,30 @@ class SignupActivity : AppCompatActivity() {
             }
 
 
-        auth.createUserWithEmailAndPassword(sgn_email.text.toString(), inputt_pass2.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    user?.sendEmailVerification()
-                        ?.addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                startActivity(Intent(this, LoginActivity::class.java))
-                                finish()
+            auth.createUserWithEmailAndPassword(
+                sgn_email.text.toString(),
+                inputt_pass2.text.toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user?.sendEmailVerification()
+                            ?.addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    startActivity(Intent(this, LoginActivity::class.java))
+                                    finish()
+                                }
                             }
-                        }
-                } else {
-                    Toast.makeText(
-                        baseContext, "Sign Up failed. Try again after some time.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    } else {
+                        Toast.makeText(
+                            baseContext, "Sign Up failed. Try again after some time.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-            }
+        }
     }
-    }
+
 
     //--------------------------------------------------------------------
 }
